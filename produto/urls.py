196569c5a3a45715351ee02118593cc7ee46b08e
leoilha <leoilhaa@gmail.com>
2024-11-24 11:46:31 -0300

    cadastrar categoria mesma pg list

--- produto/urls.py
+++ produto/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
 
+
     path('listar', views.CategoriaListView.as_view(), name='categoria_list'),
     path('save', views.CategoriaCreateView.as_view(), name='categoria_form'),
     path('update/<int:pk>', views.CategoriaUpdateView.as_view(), name='update'),
-
-    path('delete/<int:pk>', views.CategoriaDeleteView.as_view(), name='delete')
+    path('delete/<int:pk>', views.CategoriaDeleteView.as_view(), name='delete'),
 
 ]
