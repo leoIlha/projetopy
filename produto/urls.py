815ea7144b43b11283b9c40e2139ad8c032db87f
leoilha <leoilhaa@gmail.com>
2024-11-24 14:19:12 -0300

    barra pesquisa

--- produto/urls.py
+++ produto/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 
 
-    path('listar', views.CategoriaListView.as_view(), name='categoria_list'),
+    path('listarcategorias', views.CategoriaListView.as_view(), name='categoria_list'),
     path('save', views.CategoriaCreateView.as_view(), name='categoria_form'),
     path('update/<int:pk>', views.CategoriaUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', views.CategoriaDeleteView.as_view(), name='delete'),
